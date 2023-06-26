6c2cc50da69a744ada7e95e8bf174106e9fc1ff7
ravirik <ravi.rick@outlook.com>
2023-06-27 03:40:13 +0530

    commited by ricky

---
+++ pattern7.py
@@ -0,0 +1,13 @@
+def pattern7(n):
+    for i in range (n):
+        for j in range(0,n-i-1):
+            print(' ', end=' ')
+        for j in range(0,2*i+1):
+            print("*", end=' ')
+        for j in range(0,n-i-1):
+            print(' ',end=' ')
+        print("\r")
+
+n = int (10)
+
+pattern7(n)
