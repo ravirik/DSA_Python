a04a19a4563c9c14d7e5f341279551f73ddcf02a
ravirik <ravi.rick@outlook.com>
2023-07-11 04:49:58 +0530

    commited by ravi

---
+++ pattern21.py
@@ -0,0 +1,11 @@
+def pattern21(n):
+    for i in range(0,n):
+        for j in range(0,n):
+            if (i==0 or j==0 or i==(n-1) or j==(n-1)):
+                print('*',end=' ')
+            else:
+                print(' ',end=' ')
+        print("\r")
+
+n= int(input('Enter a number:'))
+pattern21(n)        
