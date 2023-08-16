c95be6104049b59fc5d4bbf1f791e56ffea0a084
ravirik <ravi.rick@outlook.com>
2023-08-16 05:43:37 +0530

    commited by ravi

---
+++ maths/gcd_hcf_2.py
@@ -0,0 +1,15 @@
+def gcd_hcf_2(a,b):
+    while(a>0 and b>0):
+        if (a>b):
+            a=a%b
+        else:
+            b=b%a
+    if(a==0):
+        print(b)
+    else:
+        print(a)
+
+a = int(input('Enter a number : '))
+b = int(input('Enter a number : '))
+
+gcd_hcf_2(a,b)
