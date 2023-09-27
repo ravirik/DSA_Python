97794e26780daf1a235b76d21b30713e1e88aaa3
ravirik <ravi.rick@outlook.com>
2023-09-27 13:45:28 +0530

    commited by ravi

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-for i in range (4,2):
-    print(i)
+def print_some(i,cnt=0):
+    if(i<1):
+        print(cnt)
+        return
+    print_some(i-1,cnt+i)
+
+
+n = int(input('Enter a number : '))
+print_some(n)
